541c18a2976ccdfd35d1ca99643f672114a7261f
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-11-05 22:10:03 -0800

    solve problem 4

--- pe4.rb
+++ pe4.rb
@@ -1,3 +1,30 @@
-# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-#
-# Find the largest palindrome made from the product of two 3-digit numbers.
+class PalinDoom
+  attr_accessor :final_array
+
+  def initialize
+    @final_array = []
+    @first_array = [*100..999]
+    @second_array = [*10..999]
+  end
+
+  def is_palindrome?(product)
+    product.to_s == product.to_s.reverse ? true : false
+  end
+
+
+  def three_digits
+    @first_array.each do |number|
+      @second_array.each do |num|
+        @test_pal = number * num
+        if is_palindrome?(@test_pal)
+          @final_array << @test_pal
+        end
+      end
+    end
+  end
+
+end
+
+palindrome = PalinDoom.new
+palindrome.three_digits
+palindrome.final_array.sort

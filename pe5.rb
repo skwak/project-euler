2b674972165772fdd967fe706d0363f316881c11
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-01-15 16:25:44 -0800

    work on least common multiple

--- pe5.rb
+++ pe5.rb
@@ -5,26 +5,41 @@
 #
 # The LCM of (1,2,3,4,5,6,7,8,9,10) = 2520
 
-def greatest_common_factor(a, b)
-  factors = []
-  factors2 = []
-  factors3 = []
-  (2..a).each do |num|
-    factors << num if a % num == 0
+  def greatest_common_factor(a, b)
+    @factors = []
+    @factors2 = []
+    @factors3 = []
+    (2..a).each do |num|
+      if a != num
+        @factors << num if a % num == 0
+      end
+    end
+
+    (2..b).each do |num|
+      if b != num
+        @factors2 << num if b % num == 0
+      end
+    end
+
+    @factors.each do |num|
+      @factors3 << num if @factors2.include?(num)
+    end
+    
+    @max_factor = @factors3.max
+    @factors3
   end
 
-  (2..b).each do |num|
-    factors2 << num if b % num == 0
-  end
-
-  factors.each do |num|
-    factors3 << num if factors2.include?(num)
-  end
-
-  @max_factor = factors3.max
-end
-
-def least_common_multiple
-  
-end
+    def least_common_multiple(arr)
+      @arr = arr
+      @arr.each do |num|
+        (2..num-1).each do |n|
+          if num % n == 0
+           #figure out how to push n more than once if necessary
+            @arr << n
+            @arr.delete(num)
+          end
+          @arr
+        end
+      end 
+    end  
 

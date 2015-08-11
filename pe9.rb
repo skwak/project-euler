b92d1ea4602ee731314ce38abc301580535363c2
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2015-08-10 22:36:34 -0700

    add working solution to problem #9

--- pe9.rb
+++ pe9.rb
@@ -5,3 +5,20 @@
 
     #There exists exactly one Pythagorean triplet for which a + b + c = 1000.
     #Find the product abc.
+
+def find_that_triplet
+  limit = 1000
+  (1..(limit/3)).each do |num|
+    a = num
+    (a...(limit/2)).each do |n|
+      b = n
+      c = limit - a - b 
+      
+      if  ((a**2) + (b**2) == (c**2)) && (a + b + c == 1000)
+        puts a * b * c 
+      end
+    end
+  end
+end
+
+find_that_triplet
